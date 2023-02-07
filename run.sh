464d48bd097230b8c5aa5fdceb696fb43c9f5051
Mihails M <mail@del.cx>
2023-02-07 00:58:42 +0000

    leak sanitizer with supression list (added by run.sh), generic leaks_blacklist, CmakeLists now has debug flags on, generate.sh can be used granulally now to only generate DEBUG / RELEASE / UNIT or ALL

--- run.sh
+++ run.sh
@@ -14,17 +14,17 @@ if [ "$2" == "RELEASE" ]; then
     cd build/release
     clear
     echo "Running in RELEASE mode."
-    ninja && $1
+    ninja && LSAN_OPTIONS=suppressions=leaks_blacklist $1
 elif [ "$2" == "DEBUG" ]; then
     cd build/debug
     clear
     echo "Running in DEBUG mode."
-    ninja && $1
+    ninja && LSAN_OPTIONS=suppressions=leaks_blacklist $1
 elif [ "$2" == "UNIT" ]; then
     cd build/unit
     clear
     echo "Running in UNIT test mode."
-    ninja && $1
+    ninja && LSAN_OPTIONS=suppressions=leaks_blacklist $1
 else
   echo "Invalid argument: $2"
 fi
