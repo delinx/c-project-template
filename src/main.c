232464a6abf75c539bb3f6cba922441a0fe080de
Mihails M <mail@del.cx>
2023-02-07 01:34:20 +0000

    template for suppressing libraries with warningr

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#endif
+// Libraries causing warning can be added here to suppress them
+// #include "raylib.h"
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
 #include "types.h"
 #include <stdio.h>
 
